96d23a7c8fb96b71957946b7b3dee7c445466d43
João Pedro <jpcabana1@gmail.com>
2020-10-07 14:23:16 -0300

    init

---
+++ demo/demo/src/main/java/com/example/demo/ApiPostgreSqlApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPostgreSqlApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ApiPostgreSqlApplication.class, args);
+	}
+
+}
